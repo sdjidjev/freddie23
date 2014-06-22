b8c4b70d912bc471c6fea1919e0b3fa1647a2d2a
Stephanie Djidjev <sdjidjev@gmail.com>
2014-06-21 22:21:55 -0600

    made width of chameleon movement more accurate

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
 		else if(e.keyCode == 39 && startGame) { 
 			$('#chameleon-pic').attr('src', 'chameleon-right.png');
 			$('#tongue').css('left','130px');
-			if (($('.chameleon-container').position().left / $('.chameleon-container').parent().width() * 100) <= 89) {
+			if (($('.chameleon-container').position().left <= window.screen.availWidth - 150) {
 				$('.chameleon-container').css('left', String($('.chameleon-container').position().left+20)+"px");
 			}
 		}
